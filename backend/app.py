c6c7cfe78c2be34445171a0444263831a1f18732
Ahmed Abuzaid <ahmedabuzaid316@gmail.com>
2025-06-16 18:52:21 +0300

    Add list of installed packages for each VM and update all files

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,51 @@
 # ~/backend_project/app.py
+#from flask import Flask, jsonify
+
+#app = Flask(__name__)
+
+#@app.route('/status')
+#def status():
+#    return jsonify({"status": "Backend is running", "author": "Ahmed Abuzaid"})
+
+#@app.route('/db')
+#def db_check():
+#    return jsonify({"db_status": "Not connected yet"})  # Will integrate later
+
+#if __name__ == '__main__':
+ #   app.run(host='0.0.0.0', port=5000)
+
+from flask_cors import CORS
 from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
+CORS(app)
+
+
+# DB connection config
+db_config = {
+    'host': '192.168.56.103',     # IP of db-vm
+    'user': 'ahmed',
+    'password': 'ahmed',               # Empty by default for MariaDB
+    'database': 'myapp'
+}
 
 @app.route('/status')
 def status():
-    return jsonify({"status": "Backend is running", "author": "Ahmed Abuzaid"})
+    return jsonify({"status": "Backend is running"})
 
-@app.route('/db')
-def db_check():
-    return jsonify({"db_status": "Not connected yet"})  # Will integrate later
+@app.route('/users')
+def get_users():
+    try:
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM users")
+        users = cursor.fetchall()
+        cursor.close()
+        conn.close()
+        return jsonify(users)
+    except Exception as e:
+        return jsonify({"error": str(e)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
---
+++ database/init.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE myapp;
+
+USE myapp;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO users (name, email) VALUES
+('Ahmed Abuzaid', 'ahmed@example.com'),
+('DevOps User', 'devops@example.com');
+
